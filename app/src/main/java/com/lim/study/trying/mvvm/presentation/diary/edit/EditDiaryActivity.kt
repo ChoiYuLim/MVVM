fc400a440e1f1cf921bae83bed18a8e75d54241c
최유림 <chzh1323@gmail.com>
2022-04-03 16:15:24 +0900

    [INIT] Init Project

---
+++ app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/edit/EditDiaryActivity.kt
@@ -0,0 +1,27 @@
+package com.lim.study.trying.mvvm.presentation.diary.edit
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lim.study.trying.mvvm.databinding.ActivityEditDiaryBinding
+
+class EditDiaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditDiaryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityEditDiaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSubmit.setOnClickListener {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+    }
+
+    companion object {
+        const val KEY_DIARY = "KEY_DIARY"
+    }
+}
