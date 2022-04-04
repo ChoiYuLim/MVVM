148a2719968f5a6c9284b97ec4c5a51d0b58418d
최유림 <chzh1323@gmail.com>
2022-04-05 00:49:44 +0900

    [ADD] by viewModels() 사용

--- app/src/main/java/com/lim/study/trying/mvvm/presentation/LiveDataTestActivity.kt
+++ app/src/main/java/com/lim/study/trying/mvvm/presentation/LiveDataTestActivity.kt
@@ -1,6 +1,7 @@
 package com.lim.study.trying.mvvm.presentation
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.lim.study.trying.mvvm.databinding.ActivityLiveDataTestBinding
 
@@ -8,12 +9,14 @@ class LiveDataTestActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLiveDataTestBinding
 
-    private val liveDataTest = LiveDataViewModel()
+//  private val liveDataTest = LiveDataViewModel()
+    private val liveDataViewModel: LiveDataViewModel by viewModels()    // 단순하게 뷰모델을 주입받는 것은 생성자에 아무것도 없어야한다 (LiveDataViewModel에 생성자 아무것도 없다)
+//  private val liveDataViewModel by viewModels<LiveDataViewModel>() 위랑 같은 말
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityLiveDataTestBinding.inflate(layoutInflater)
-        binding.liveDataTest = liveDataTest
+        binding.liveDataViewModel = liveDataViewModel
         binding.lifecycleOwner = this   // 라이프사이클 오너는 액티비티가 갖고 있기 떄문에 this
 
         /* 데이터 바인딩의 바인딩 객체를 만드는 방법 2가지
