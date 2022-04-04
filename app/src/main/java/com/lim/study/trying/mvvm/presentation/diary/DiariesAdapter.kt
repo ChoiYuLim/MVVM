d27317ffb2826c8dd94c55326670bdd63bca9522
최유림 <chzh1323@gmail.com>
2022-04-04 13:03:19 +0900

    [ADD] listAdapter 구현

--- app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/DiariesAdapter.kt
+++ app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/DiariesAdapter.kt
@@ -1,6 +1,9 @@
 package com.lim.study.trying.mvvm.presentation.diary
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.lim.study.trying.mvvm.databinding.ItemDiaryBinding
 import com.lim.study.trying.mvvm.domain.Diary
@@ -9,7 +12,38 @@ import com.lim.study.trying.mvvm.domain.Diary
 // 2. diff util
 // 3. list adapter
 
-class DiariesAdapter {
+//DiariesAdapter의 생성자에 다이어리 클릭 콜백을 넣는 것을 강제해보자
+class DiariesAdapter(
+    private val onMemoClick: ((Diary) -> Unit)? = null, // 안 넣을 수도 있으니깐 안 넣으면 null이 자동으로 들어가게 default로 null 넣어주기
+) : ListAdapter<Diary, DiariesAdapter.ViewHolder>(DiariesComparator()) { //처음 타입에는 item을 넣어(Diary), 그 다음 viewHolder 넣어 (nested class 이기 때문에 DiariesAdapter.ViewHolder에서 . 을 지울 수 없음)
+
+    //원래 item 찾으려면 private val diaries = MutableListOf<Diary>() 이렇게 해서 getItem 어쩌구 이런식으로 했었는데 필요없음
+    //기존의 리싸이클러뷰를 사용했다면 아이템 목록을 직접 가지고 있어야함
+    //데이터 넘기는 코드가 하나도 없음
+    //원래 데이터 넘겨서 diaries 추가하고 notifyItemchanged 이런거 하고 했는데
+    //여기 데이터를 넘기는 방식은 submitList(list) 이게 끝
+    /* override fun submitList(list: MutableList<Diary>?) {
+        super.submitList(list)
+    } 이걸 외부에서 호출해서 넣어주기만 하면 된다 */
+    //중요한 로직들을 쓸 필요가 없다
+    //데이터를 받아서 보여주는 것은 리스트 어댑터가 직접 알아서 해주기 때문에 신경 쓰지 않고 넘겨주기만 하면 된다
+
+
+    //뷰홀더를 만들기 위해서는 바인딩이 필요할 거고 , 바인딩을 만들기 위해서는 layoutInflater가 필요
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context) //from 안에 context가 필요. inflater는 context로 부터 가지고 올 수 있는데 context는 뷰에 항상 들어있음. parent에 뷰 있으니깐 parent에서 꺼내서 쓰면 됨(parent.context)
+        val binding = ItemDiaryBinding.inflate(layoutInflater)
+        return ViewHolder(binding) //뷰홀더 리턴해야하므로
+    }
+
+    //뷰홀더에 아이템을 세팅해주는 역할
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val diary: Diary = getItem(position)// 원하는 다이어리를 가져올 수 있다 , getItem은 리스트어댑터에서 구현해놓은 메소드
+        holder.bind(diary, onMemoClick) // 뷰홀더에 데이터를 넘겨줄건데 뷰홀더에 만들어놓은 bind 함수로 넣어주면 된다
+    }
+
+    //순수 recyclerview adapter로 구현했으면 getItemCount()라는 것도 구현해줬어야한다
+    //listAdapter에서 getItemCount() 구현했기 때문에 우리가 만들 필요 없음
 
     class ViewHolder(
         private val binding: ItemDiaryBinding, // 생성자로 뷰의 바인딩 클래스를 받아
