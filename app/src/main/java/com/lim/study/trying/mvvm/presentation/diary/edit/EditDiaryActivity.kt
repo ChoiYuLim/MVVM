2992a53e3efae6c030b06e879465b49586824ce0
최유림 <chzh1323@gmail.com>
2022-04-04 13:43:48 +0900

    [DEL] EditDiaryActivity 구현하기 전 세팅

--- app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/edit/EditDiaryActivity.kt
+++ app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/edit/EditDiaryActivity.kt
@@ -21,31 +21,9 @@ class EditDiaryActivity : AppCompatActivity() {
         //그래서 inflater 안에 parser가 들어있다
         setContentView(binding.root) //여기까지 데이터바인딩 쓸 준비됨
 
-        val diary = Diary(
-            id = 0,
-            title = "제목",
-            content = "내용",
-            createDate = Date(),
-        )
-        binding.bindingString = "바인딩스트링"
-        //binding.setVariable(BR.diary, diary) //바인딩 타입을 모를 때
-        binding.diary = diary
-        binding.bindingInt = 5
-
-        /* 뷰 바인딩을 쓰면 아래와 같이 하나씩 넣어줘야하는데
-        binding.textDate.text = diary.createDate.toString()
-        binding.textTitle.setText(diary.title)
-
-        데이터 바인딩을 쓰면 이걸 xml에 한번에 넣을 수 있다
-        */
-
         binding.buttonSubmit.setOnClickListener {
             setResult(Activity.RESULT_OK)
             finish()
         }
-    }
-
-    companion object {
-        const val KEY_DIARY = "KEY_DIARY"
     }
 }
