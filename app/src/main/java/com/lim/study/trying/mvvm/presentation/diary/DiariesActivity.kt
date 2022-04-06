5d99ab4e19c80a47dc178cc5a1b19226c419f8fb
최유림 <chzh1323@gmail.com>
2022-04-06 17:27:09 +0900

    [CHORE] resultCode

--- app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/DiariesActivity.kt
+++ app/src/main/java/com/lim/study/trying/mvvm/presentation/diary/DiariesActivity.kt
@@ -1,5 +1,6 @@
 package com.lim.study.trying.mvvm.presentation.diary
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -32,7 +33,11 @@ class DiariesActivity : AppCompatActivity() {
             // 이거 하나로 권한 관련해서 깔끔하게 해결 가능 -> 이거 말리빈 블로그에 정리되어있는 거 찾아보기
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 // 2. 콜백을 람다로 집어넣기 -> 띄운 액티비티에서 setTResult를 통해서 resultok 또는 resultcancel을 받아 -> 이 결과가 콜백으로부터 들어옴 -> ActivityResult라는 객체로 부터 튀어나옴 -> 그걸 it으로 접근 가능
-                Toast.makeText(this, "result code: ${it.resultCode}", Toast.LENGTH_LONG).show()
+                when (it.resultCode) {
+                    Activity.RESULT_OK -> showToast("작성 완료!")
+                    Activity.RESULT_CANCELED -> showToast("작성 취소")
+                    else -> showToast("문제가 발생했습니다 : $it")
+                }
             } // editDiaryActivityLauncher로부터 띄운 액티비티가 종료되면 실행되는 콜백이 이 안에 들어있다
 
         //DiariesAdapter() 여기 안에 람다를 넣으면된다, 무슨 일이 일어날거냐
@@ -77,6 +82,10 @@ class DiariesActivity : AppCompatActivity() {
         val intent = Intent(this, EditDiaryActivity::class.java)
         intent.putExtra(EditDiaryActivity.KEY_DIARY_ID, diary?.id) //diary가 null이면 .id 까지도 안 오고 바로 null로 치환해줌
         editDiaryActivityLauncher.launch(intent)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 //    companion object {
